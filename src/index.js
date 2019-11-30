34794cf30def47b3e103ea3fc862d90d85c45088
Denis Hasanenko <denis.hasanenko@gmail.com>
2019-11-30 12:57:30 +0200

    add bugsnag

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
+import bugsnag from '@bugsnag/js'
+import bugsnagReact from '@bugsnag/plugin-react'
 
+const bugsnagClient = bugsnag('dd223483153168e5915e18e663a0737a');
+bugsnagClient.use(bugsnagReact, React);
+const ErrorBoundary = bugsnagClient.getPlugin('react');
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
